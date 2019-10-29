1f6615cafc15e236696448103efae9e561028af1
rc1200 <ron.calibuso@gmail.com>
2019-10-29 15:35:18 -0400

    delete button works in add stock

--- quotes/views.py
+++ quotes/views.py
@@ -6,11 +6,11 @@ from .models import Stock
 from .forms import StockFrom
 from django.contrib import messages
 
-pk = 'pk_908d3547248549428866b03ba25e03e5'
+PUBLIC_KEY = 'pk_908d3547248549428866b03ba25e03e5'
 
 
 def getStockData(ticker):
-    api_request = requests.get('https://cloud.iexapis.com/stable/stock/{}/quote?token={}'.format(str(ticker), pk))
+    api_request = requests.get('https://cloud.iexapis.com/stable/stock/{}/quote?token={}'.format(str(ticker), PUBLIC_KEY))
     try:
         api = json.loads(api_request.content)
     except Exception as e:
@@ -23,23 +23,16 @@ def home(request):
 
     if request.method == 'POST':
         ticker = request.POST['ticker']
-        # api_request = requests.get('https://cloud.iexapis.com/stable/stock/{}/quote?token={}'.format(ticker, pk))
         api_request = getStockData(ticker)
-        # print(api_request)
-
-        # try:
-        #     api = json.loads(api_request.content)
-        # except Exception as e:
-        #     api = 'Error...'
 
         return render(request,'home.html',{'returnStuff': api_request })
     else:
         return render(request,'home.html',{'ticker': 'enter a ticker symbol...'})
 
-    # return render(request,'home.html',{'returnStuff': api})
 
 def about(request):
     return render(request,'about.html',{})
+
 
 def add_stock(request):
 
@@ -52,16 +45,13 @@ def add_stock(request):
             return redirect('add_stock')
     
     else:
-        output =[]
         ticker = Stock.objects.all()
-        print(ticker)
+        myDict = {}
         
         for i in ticker:
-            # print(i)
-            # print(getStockData(i))
-            if getStockData(i) != 'Error...':
-                output.append(getStockData(i))
-        return render(request,'add_stock.html',{ 'ticker' : ticker, 'output' : output })
+            myDict[str(i.pk)] = getStockData(i)
+
+        return render(request,'add_stock.html',{'myDict' : myDict})
 
 def delete(request, stock_id):
     item = Stock.objects.get(pk=stock_id)
@@ -71,4 +61,5 @@ def delete(request, stock_id):
 
 
 def delete_stock(request):
-    return render(request,'delete_stock.html',{})
+    ticker = Stock.objects.all()
+    return render(request,'delete_stock.html',{'ticker' : ticker})
